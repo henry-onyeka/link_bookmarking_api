45eb18966bac81f848e6ac9f864359c09566197c
henry-onyeka <hmike54@gmail.com>
2025-08-31 20:08:33 +0100

    starting up

--- link_bookmarking_api/app.py
+++ link_bookmarking_api/app.py
@@ -1,2 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
 
+""" creating aan instance of flask"""
+app = Flask(__name__)
+'''activating a get method for the root page'''
+@app.get("/")
+def index():
+    return  'hell0 gamers'
+
+@app.get("/hello")
+def sayHello():
+    return jsonify({
+        "say hi": 'whatsup guys'
+    })
